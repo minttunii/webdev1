9a0468d729f471eb4a8d7c5f558715ffc959792d
Minttu Niiranen <minttu.niiranen@tuni.fi>
2023-09-09 16:17:40 +0300

    Create server excersice first try

--- exercises/02_http/2.2-create-server/index.js
+++ exercises/02_http/2.2-create-server/index.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer(function(request, response) {
-  fs.readFile(path.resolve('FILE_NAME_HERE'), function(error, htmlPage) {
+  fs.readFile(path.resolve('index.html'), function(error, htmlPage) {
     if (error) {
       response.writeHead(404);
       response.write('An error occured: ', error);
     } else {
-      response.writeHead(200, { 'Content-Type': 'FILE_TYPE_HERE' });
+      response.writeHead(200, { 'Content-Type': 'html' });
       response.write(htmlPage);
     }
     response.end();
