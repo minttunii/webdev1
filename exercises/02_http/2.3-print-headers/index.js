ea149992055c7f9c2bd97a1b8de4427ca9a6a5ee
Minttu Niiranen <minttu.niiranen@tuni.fi>
2023-09-10 14:52:26 +0300

    Print request headers second try

--- exercises/02_http/2.3-print-headers/index.js
+++ exercises/02_http/2.3-print-headers/index.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const port = 3000
 
 server = http.createServer(function(req, res){
-    res.writeHead(200, { 'Content type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(JSON.stringify(req.headers));
     res.end();
 });
+
+server.listen(port);
