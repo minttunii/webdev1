0b1747af66f34c9691e07e2f0099e1db8764cd7e
Minttu Niiranen <minttu.niiranen@tuni.fi>
2023-09-11 16:36:09 +0300

    2.7 api testing executed

---
+++ exercises/02_http/2.7-api-testing/index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs');
+const path = require('path');
+
+module.exports = http.createServer(function(req, res){
+    if(req.url.includes('/homer')){
+        readFileSendResponse('homer.html', 'text/html', res);
+    }
+    else if(req.url.includes('/bradbury')){
+        readFileSendResponse('bradbury.html', 'text/html', res);
+    }
+    else if(req.url == '/'){
+        readFileSendResponse('index.html', 'text/html', res);
+    }
+    else{
+        res.statusCode = 404;
+        res.statusMessage = 'Requested content not found';
+        res.end();
+    }
+}).listen(3000);
+
+
+/* 
+  * @param {string} fileName - name of the file to be read
+  * @param {string} contentType - type of the content to be sent in the response
+  * @param {object} response - response object
+  */
+const readFileSendResponse = (fileName, contentType, response) => {
+    fs.readFile(path.resolve(fileName), function (error, file) {
+      if (error) {
+        response.writeHead(404);
+        response.write('An error occured: ', error);
+      } else {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.write(file);
+      }
+      response.end();
+    })
+  }
+
--- exercises/02_http/2.7-api-testing/test/api.js
+++ exercises/02_http/2.7-api-testing/test/api.js
@@ -52,26 +52,16 @@ describe('Going through the routes', () => {
    * Test the /homer route, should receive homer.html
    */
   describe('GET /homer path', () => {
-    // TODO modify the it statement to use the function that has been 
-    // commented out below.
-    // You also need to uncomment the needed parts.
-    // As the result the status of the test should go from 'pending' to 'passing'
-    // Even thou just leaving the function inside <em>end</em> method will pass,
-    //  you must write the tests as described in the TODO inside the end method.
-    it('it should GET the homer.html')
-    // TODO uncomment the needed parts
-    // BE EXTRA CAREFUL WITH THE PARANTHESES WHEN UNCOMMENTING! SEE THE EXAMPLES ABOVE!
-    // , (done) => {
-    //     chai.request(server)
-    //         .get('/homer')
-    //         .end((err, res) => {
-    //             // TODO: test that 
-    //             // a) the response should have HTTP response status of 200, and
-    //             // b) that the response is in HTML form, and
-    //             // c) that the text of the response is equal to homer.html, so here the response text should be equal to const homer
-    //             done();
-    //         });
-    // });
+    it('it should GET the homer.html', (done) => {
+      chai.request(server)
+        .get('/homer')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          res.text.should.be.eql(homer);
+          done();
+        });
+    });
   });
 
   /*
@@ -84,20 +74,15 @@ describe('Going through the routes', () => {
     // As the result the status of the test should go from 'pending' to 'passing'
     // Even thou just leaving the function inside <em>end</em> method will pass,
     //  you must write the tests as described in the TODO inside the end method.
-    it('it should GET the bradbury.html')
-    // BE EXTRA CAREFUL WITH THE PARANTHESES WHEN UNCOMMENTING! SEE THE EXAMPLES ABOVE!
-    //     , (done) => {
-    //         chai.request(server)
-    //             .get('/bradbury')
-    //             .end((err, res) => {
-    //                 // TODO: test that 
-    //                 // a) the response should have HTTP response status of 200, 
-    //                 // and
-    //                 // b) that the response is in HTML form
-    //                 // c) the text of the response is equal to bradbury.html,  
-    //                      so here the response text should be equal to const bradbury
-    //                 done();
-    //             });
-    //     });
+    it('it should GET the bradbury.html', (done) => {
+      chai.request(server)
+        .get('/bradbury')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          res.text.should.be.eql(bradbury);
+          done();
+        });
+    });
   });
 });
