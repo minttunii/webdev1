c748d922fbaf26a03e0ce517645a91011c7a0ea1
Minttu Niiranen <minttu.niiranen@tuni.fi>
2023-09-18 13:31:45 +0300

    Few fixes

--- exercises/03_html/3.6-html-template-tag/template.js
+++ exercises/03_html/3.6-html-template-tag/template.js
@@ -1,19 +1,20 @@
 contacts = document.getElementById("contacts");
-form = document.getElementById("form");
-template = document.querySelector("#contact-template");
+const form = document.getElementById("form");
+const template = document.getElementById("contact-template");
 
-form.getElementById("submit").addEventListener("click", function(event){
+ form.addEventListener("submit", function(event){
     event.preventDefault();
-    // Get form data
+    // Get the form data
     formData = new FormData(form);
     // Make clone of template and add data from form into the clone
-    const clone = template.content.cloneNode(true);
+    clone = document.importNode(template.content, true);
     // Name
-    clone.getElementByTagName("h2").textContent = formData.get("name");
+    clone.querySelector("h2").textContent = formData.get("name");
     // Email
-    clone.getElementByClass("email").textContent = formData.get("email");
+    clone.querySelector(".email").textContent = formData.get("email");
     // Homepage
-    clone.getElementByClass("homepage").href = formData.get("homepage");
+    clone.querySelector(".homepage a").textContent = formData.get("email");
+    clone.querySelector(".homepage a").setAttribute("href", formData.get("homepage"));
     // Append clone into contacts
     contacts.appendChild(clone);
     form.reset();
